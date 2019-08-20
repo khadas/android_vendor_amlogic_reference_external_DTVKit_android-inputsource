7cb30a4b3da30a5955c3f46dd055584c10e80bc0
Zhiqiang Han <zhiqiang.han@amlogic.com>
2019-08-20 14:44:33 +0800

    update: enable channel update [2/2]
    PD#SWPL-11659
    
    Problem:
    update is disabled for channeldata
    
    Solution:
    enable update of channel data
    
    Verify:
    Patchbuild on R314
    
    Change-Id: I3f5e94a91c7d092cd00c88c29ecfe16b7909c0e0
    Signed-off-by: Zhiqiang Han <zhiqiang.han@amlogic.com>

--- app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
@@ -1588,7 +1588,7 @@ public class DtvkitTvInput extends TvInputService {
                     Log.i(TAG, "DvbUpdatedChannel");
                     ComponentName sync = new ComponentName(mContext, DtvkitEpgSync.class);
                     EpgSyncJobService.requestImmediateSync(mContext, mInputId, false, true, sync);
-                }/*
+                }
                 else if (signal.equals("DvbUpdatedChannelData"))
                 {
                     Log.i(TAG, "DvbUpdatedChannelData");
@@ -1600,7 +1600,7 @@ public class DtvkitTvInput extends TvInputService {
                     Log.i(TAG, "audio track selected: " + playerGetSelectedAudioTrack());
                     notifyTrackSelected(TvTrackInfo.TYPE_AUDIO, Integer.toString(playerGetSelectedAudioTrack()));
                     initSubtitleOrTeletextIfNeed();
-                }*/
+                }
                 else if (signal.equals("MhegAppStarted"))
                 {
                    Log.i(TAG, "MhegAppStarted");
