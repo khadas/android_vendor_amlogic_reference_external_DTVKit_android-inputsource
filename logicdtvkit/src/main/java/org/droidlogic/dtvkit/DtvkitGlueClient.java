91271c68833dc75eec2fab07330b3359228019a0
junchao yuan <junchao.yuan@amlogic.com>
2020-04-29 13:32:29 +0800

    inputsource: fix no subtitle show in small window [1/4]
    PD#SWPL-24688
    
    Problem:
    no subtitle show in small window;
    mheg display error;
    
    Solution:
    draw subtitle with android bitmap;
    Revert "inputsource: draw subtitle, teletext, mheg with opengl [1/3]"
    
    Verify:
    u215
    
    Change-Id: Id4a79e25db7925533feaba045973c1227a45a16e
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- logicdtvkit/src/main/java/org/droidlogic/dtvkit/DtvkitGlueClient.java
+++ logicdtvkit/src/main/java/org/droidlogic/dtvkit/DtvkitGlueClient.java
@@ -11,7 +11,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.view.Surface;
 import android.view.SurfaceHolder;
-import android.graphics.SurfaceTexture;
 
 import java.util.ArrayList;
 
@@ -20,6 +19,12 @@ public class DtvkitGlueClient {
 
     private static DtvkitGlueClient mSingleton = null;
     private ArrayList<SignalHandler> mHandlers = new ArrayList<>();
+    // Notification object used to listen to the start of the rpcserver daemon.
+    //private final ServiceNotification mServiceNotification = new ServiceNotification();
+    //private static final int DTVKITSERVER_DEATH_COOKIE = 1000;
+   // private IDTVKitServer mProxy = null;
+    //private HALCallback mHALCallback;
+    private OverlayTarget mTarget;
     private AudioHandler mAudioHandler;
     private SystemControlHandler mSysControlHandler;
     // Mutex for all mutable shared state.
@@ -28,10 +33,18 @@ public class DtvkitGlueClient {
     private native void nativedisconnectdtvkit();
     private native void nativeSetSurface(Surface surface);
     private native String nativerequest(String resource, String json);
-    private native void nativesetGraphicBufferProducer(SurfaceTexture surface);
 
     static {
         System.loadLibrary("dtvkit_jni");
+    }
+
+        //native callback
+    public void notifySubtitleCallback(int width, int height, int dstx, int dsty, int dstwidth, int dstheight, byte[] data)
+    {
+         Log.d(TAG, "notifySubtitleCallBack received!!! width = " + width + ", heigth = " + height);
+         if (mTarget != null) {
+            mTarget.draw(width, height, dstx, dsty, dstwidth, dstheight, data);
+         }
     }
 
     public void notifyDvbCallback(String resource, String json) {
@@ -50,17 +63,82 @@ public class DtvkitGlueClient {
         }
     }
 
-    public void setDisplay(Surface sh) {
+   public void setDisplay(Surface sh) {
         nativeSetSurface(sh);
-    }
+   }
 
-    public void setGraphicBufferProducer(SurfaceTexture surface) {
-        nativesetGraphicBufferProducer(surface);
-    }
-
-    public void disConnectDtvkitClient() {
+   public void disConnectDtvkitClient() {
         nativedisconnectdtvkit();
+   }
+
+
+/*
+    final class ServiceNotification extends IServiceNotification.Stub {
+        @Override
+        public void onRegistration(String fqName, String name, boolean preexisting) {
+            Log.i(TAG, "rpcserver HIDL service started " + fqName + " " + name);
+            connectToProxy();
+        }
     }
+
+    private void connectToProxy() {
+        synchronized (mLock) {
+            if (mProxy != null) {
+                return;
+            }
+
+            try {
+                mProxy = IDTVKitServer.getService();
+                mProxy.linkToDeath(new DeathRecipient(), DTVKITSERVER_DEATH_COOKIE);
+                mProxy.setCallback(mHALCallback, ConnectType.TYPE_EXTEND);
+            } catch (NoSuchElementException e) {
+                Log.e(TAG, "connectToProxy: DTVKitServer HIDL service not found."
+                        + " Did the service fail to start?", e);
+            } catch (RemoteException e) {
+                Log.e(TAG, "connectToProxy: DTVKitServer HIDL service not responding", e);
+            }
+        }
+
+        Log.i(TAG, "connect to DTVKitServer HIDL service success");
+    }
+
+    private static class HALCallback extends IDTVKitServerCallback.Stub {
+        DtvkitGlueClient DtvkitClient;
+        HALCallback(DtvkitGlueClient dkgc) {
+            DtvkitClient = dkgc;
+    }
+
+    public void notifyCallback(DTVKitHidlParcel parcel) {
+        Log.i(TAG, "resource" + parcel.resource + ",json" + parcel.json);
+        JSONObject object;
+        try {
+            object = new JSONObject(parcel.json);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            return;
+        }
+
+        for (SignalHandler handler : DtvkitClient.mHandlers) {
+            handler.onSignal(parcel.resource, object);
+        }
+        }
+    }
+
+    final class DeathRecipient implements HwBinder.DeathRecipient {
+        DeathRecipient() {
+        }
+
+        @Override
+        public void serviceDied(long cookie) {
+            if (DTVKITSERVER_DEATH_COOKIE == cookie) {
+                Log.e(TAG, "dtvkitserver HIDL service died cookie: " + cookie);
+                synchronized (mLock) {
+                    mProxy = null;
+                }
+            }
+        }
+    }
+*/
 
     public interface AudioHandler {
        void onEvent(String signal, JSONObject data);
@@ -68,6 +146,10 @@ public class DtvkitGlueClient {
 
     public interface SignalHandler {
         void onSignal(String signal, JSONObject data);
+    }
+
+    public interface OverlayTarget {
+        void draw(int src_width, int src_height, int dst_x, int dst_y, int dst_width, int dst_height, byte[] data);
     }
 
     protected DtvkitGlueClient() {
@@ -96,6 +178,10 @@ public class DtvkitGlueClient {
                 mHandlers.remove(handler);
             }
         }
+    }
+
+    public void setOverlayTarget(OverlayTarget target) {
+        mTarget = target;
     }
 
     public JSONObject request(String resource, JSONArray arguments) throws Exception {
