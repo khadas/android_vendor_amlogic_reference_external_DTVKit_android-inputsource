96274f418568113ffaa45fa02bc3b29afe1bad90
kaifu.hu <kaifu.hu@amlogic.com>
2021-07-02 11:45:21 +0800

    dtvkit-TIS: add dtvkit afd [1/1]
    PD#SWPL-53591
    
    Problem:
    afd error
    
    Solution:
    add dtvkit afd
    
    Verify:
    ok on redi.
    
    Signed-off-by: kaifu.hu <kaifu.hu@amlogic.com>
    Change-Id: I6ea1fd7d0ff245b889c6d712a7422c2b7d3749a4

--- app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
@@ -2523,6 +2523,7 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                             setOverlayViewEnabled(false);
                             mHardware.setSurface(null, null);
                             if (mSystemControlManager != null) {
+                                mSystemControlManager.SetDtvKitSourceEnable(0);
                                 mSystemControlManager.SetCurrentSourceInfo(SystemControlManager.SourceInput.valueOf(INPUT_MPEG), 0, 0);
                             }
                             //sendSetSurfaceMessage(null, null);
@@ -2530,7 +2531,6 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                             mSurface = null;
                             sendDoReleaseMessage();
                             writeSysFs("/sys/class/video/video_inuse", "0");
-                            mSystemControlManager.SetDtvKitSourceEnable(0);
                         }
                     } else {
                         if (mSurface != null && mSurface != surface) {
@@ -2541,11 +2541,11 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                         }
                         mHardware.setSurface(surface, mConfigs[0]);
                         if (mSystemControlManager != null) {
+                            mSystemControlManager.SetDtvKitSourceEnable(1);
                             mSystemControlManager.SetCurrentSourceInfo(SystemControlManager.SourceInput.valueOf(INPUT_DVB), 0, 0);
                         }
                         //sendSetSurfaceMessage(surface, mConfigs[0]);
                         mSurface = surface;
-                        mSystemControlManager.SetDtvKitSourceEnable(1);
                     }
                 }
                 //set surface to mediaplayer
