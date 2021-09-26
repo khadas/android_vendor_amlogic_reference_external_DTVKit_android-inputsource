d33a0f76af90f203ca12db36cc0a7cde5c235e8d
kieth liu <kieth.liu@amlogic.com>
2021-09-26 15:09:46 +0800

    dtvkit-TIS: set dtv source to PQ when PIP [1/1]
    PD#SWPL-60380
    
    Problem:
    aspect mode no effect when PIP
    
    Solution:
    set dtv source to PQ when PIP
    
    Verify:
    redi
    
    Change-Id: Id341873c163d26bafacaa3ea467620d57eee7283
    Signed-off-by: kieth liu <kieth.liu@amlogic.com>

--- app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitTvInput.java
@@ -2718,6 +2718,10 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                         }
                     }
                     //sendDoReleaseMessage();
+                    if (mSystemControlManager != null) {
+                        mSystemControlManager.SetDtvKitSourceEnable(0);
+                        mSystemControlManager.SetCurrentSourceInfo(SystemControlManager.SourceInput.valueOf(INPUT_MPEG), 0, 0);
+                    }
                     doRelease(false, false);
                 }
                 if (mSurface != surface) {
@@ -2967,6 +2971,10 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                     if (isSdkAfterAndroidQ()) {
                         mHardware.setSurface(mSurface, mConfigs[1]);
                         setSurfaceTunnelId(INDEX_FOR_MAIN, 1);
+                        if (mSystemControlManager != null) {
+                            mSystemControlManager.SetDtvKitSourceEnable(1);
+                            mSystemControlManager.SetCurrentSourceInfo(SystemControlManager.SourceInput.valueOf(INPUT_DVB), 0, 0);
+                        }
                     } else {
                         DtvkitGlueClient.getInstance().setMutilSurface(INDEX_FOR_MAIN, mSurface);
                     }
@@ -5369,6 +5377,10 @@ public class DtvkitTvInput extends TvInputService implements SystemControlEvent.
                     if (isSdkAfterAndroidQ()) {
                         mHardware.setSurface(mSurface, mConfigs[1]);
                         setSurfaceTunnelId(INDEX_FOR_MAIN, 1);
+                        if (mSystemControlManager != null) {
+                            mSystemControlManager.SetDtvKitSourceEnable(1);
+                            mSystemControlManager.SetCurrentSourceInfo(SystemControlManager.SourceInput.valueOf(INPUT_DVB), 0, 0);
+                        }
                     } else {
                         DtvkitGlueClient.getInstance().setMutilSurface(INDEX_FOR_MAIN, mSurface);
                     }
