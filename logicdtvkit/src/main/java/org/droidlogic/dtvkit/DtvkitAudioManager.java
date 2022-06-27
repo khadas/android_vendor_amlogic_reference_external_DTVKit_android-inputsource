b5a4254c95384fec086bb172ae59805622082292
S_F_Qiang <shaofeng.qiang@amlogic.com>
2022-06-27 17:19:02 +0800

    inputsource: throw exception when input param is invalid [1/1]
    PD#TV-50523
    
    Problem:
    custom suggestion
    
    Solution:
    throw exception when input param is invalid
    
    Verify:
    redi
    
    Change-Id: Ifb67b08c902cfdefdff3045bcd5122e9a189989b
    Signed-off-by: S_F_Qiang <shaofeng.qiang@amlogic.com>

--- logicdtvkit/src/main/java/org/droidlogic/dtvkit/DtvkitAudioManager.java
+++ logicdtvkit/src/main/java/org/droidlogic/dtvkit/DtvkitAudioManager.java
@@ -1,11 +1,14 @@
 package org.droidlogic.dtvkit;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 
-import org.droidlogic.dtvkit.DtvkitGlueClient;
 import org.json.JSONArray;
 
 /**
@@ -13,14 +16,16 @@ import org.json.JSONArray;
  */
 public class DtvkitAudioManager {
     private static final String TAG = "DtvkitAudioManager";
+    private Context mContext;
+    private final String PREFS_NAME = "dtvkit_audio";
     AudioManager audioManager;
-
     public enum EntryType {
         DIALOGUE_ENHANCEMENT_LEVEL,
     }
 
     public DtvkitAudioManager(Context context) {
         audioManager = context.getSystemService(AudioManager.class);
+        mContext = context;
     }
 
     /**
@@ -44,14 +49,15 @@ public class DtvkitAudioManager {
                         audioManager.setParameters("ms12_runtime=-dap_dialogue_enhancer 1,4");
                         audioManager.setParameters("ms12_runtime=-ac4_de 4");
                     } else {
-                        Log.e(TAG, "invalid level:" + level + " switchOn " + type);
+                        throw new IllegalArgumentException("level in params is invalid:" + level);
                     }
+                    setAudioInfo(type.toString(), level);
                 } else {
-                    Log.e(TAG, "params should putString(\"level\", [\"slow\"|\"medium\"|\"high\"]) when switchOn " + type);
+                    throw new IllegalArgumentException("params is null");
                 }
                 break;
             default:
-                Log.e(TAG, "Unsupport switchOn " + type);
+                Log.e(TAG, "Unsupported switchOn " + type);
                 break;
         }
     }
@@ -67,11 +73,40 @@ public class DtvkitAudioManager {
                 audioManager.setParameters("ms12_runtime=-dap_dialogue_enhancer 0,0");
                 audioManager.setParameters("ms12_runtime=-dap_dialogue_enhancer 1,0");
                 audioManager.setParameters("ms12_runtime=-ac4_de 0");
+                setAudioInfo(type.toString(), null);
                 break;
             default:
-                Log.e(TAG, "Unsupport switchOff " + type);
+                Log.e(TAG, "Unsupported switchOff " + type);
                 break;
         }
+    }
+
+    /**
+     * getValue of the spec type.
+     *
+     * @param type the special type to get
+     */
+    public Bundle getValue(EntryType type) {
+        Bundle b = new Bundle();
+        switch (type) {
+            case DIALOGUE_ENHANCEMENT_LEVEL:
+                String _switch = getAudioInfo(type.toString());
+                if (_switch == null) {
+                    b.putString("switch", "off");
+                } else {
+                    if (TextUtils.equals("off", _switch)) {
+                        b.putString("switch", "off");
+                    } else {
+                        b.putString("switch", "on");
+                        b.putString("level", _switch);
+                    }
+                }
+                return b;
+            default:
+                Log.e(TAG, "Unsupported switchOff " + type);
+                break;
+        }
+        return null;
     }
 
     /**
@@ -111,4 +146,19 @@ public class DtvkitAudioManager {
         }
     }
 
+    private String getAudioInfo(String key){
+        SharedPreferences sp = mContext.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        return sp.getString(key, null);
+    }
+
+    private void setAudioInfo(String key, String value){
+        SharedPreferences sp = mContext.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        if (value == null) {
+            editor.remove(key);
+        } else {
+            editor.putString(key, value);
+        }
+        editor.apply();
+    }
 }
