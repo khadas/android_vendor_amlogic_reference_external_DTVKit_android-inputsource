e68ab128f44177507d72fddff6212d4e765cb53e
ming.an <ming.an@amlogic.com>
2022-09-07 17:03:28 +0800

    fvp: add FVP feature permission [1/1]
    PD#SWPL-92837
    
    Problem:
    can not open FVP app
    
    Solution:
    add FVP feature permission
    
    Verify:
    smith
    
    Change-Id: Ie15a3a1c5d48eb10345592ec873003a4f8531fa6
    Signed-off-by: ming.an <ming.an@amlogic.com>

--- fvp/src/main/Android.mk
+++ fvp/src/main/Android.mk
@@ -22,3 +22,13 @@ LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := freeview.software.androidtv.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/permissions
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_PREBUILT)
+
