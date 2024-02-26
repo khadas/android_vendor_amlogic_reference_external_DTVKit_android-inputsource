d7bd5aee6b9b10d6429d8ebc47f33a33b0169e54
kai.qin <kai.qin@amlogic.com>
2024-02-26 15:07:09 +0800

    Rating: Remove range limitation of rating [1/1]
    PD#TV-111401
    
    Problem:
    For dvb, we have limited the rating age in 4 - 19
    when building TvContentRating.
    Some rating systems has special values
    that not limited in this range.
    
    Solution:
    Remove the limitation, live tv will handle it with
    different rating system.
    
    Verify:
    oppencas
    
    Change-Id: I07441fecee7770b57fed9e6a5176b5e869039bc0
    Signed-off-by: kai.qin <kai.qin@amlogic.com>

--- app/src/main/java/org/dtvkit/inputsource/DtvkitEpgSync.java
+++ app/src/main/java/org/dtvkit/inputsource/DtvkitEpgSync.java
@@ -782,16 +782,13 @@ public class DtvkitEpgSync extends EpgSyncJobService {
             String[] DVB_ContentRating = {"DVB_4", "DVB_5", "DVB_6", "DVB_7", "DVB_8", "DVB_9", "DVB_10",
                     "DVB_11", "DVB_12", "DVB_13", "DVB_14", "DVB_15", "DVB_16", "DVB_17", "DVB_18", "DVB_19"};
 
-            parentalRating += 3; //minimum age = rating + 3 years
-//        Log.d(TAG, "parseParentalRatings parentalRating:"+ parentalRating + ", title = " + title);
-            if (parentalRating >= 4 && parentalRating <= 19) {
-                TvContentRating r = TvContentRating.createRating(ratingDomain, ratingSystemDefinition, DVB_ContentRating[parentalRating - 4], (String) null);
-                if (r != null) {
-                    ratings_array[0] = r;
-                    Log.d(TAG, "parse ratings add rating:" + r.flattenToString() + ", title = " + title);
-                }
-            } else {
+            if (parentalRating == 0) {
+                //no rating
                 ratings_array = null;
+            } else {
+                TvContentRating r = TvContentRating.createRating(ratingDomain,
+                    ratingSystemDefinition, "DVB_" + (parentalRating + 3), (String) null);
+                ratings_array[0] = r;
             }
         }
 
